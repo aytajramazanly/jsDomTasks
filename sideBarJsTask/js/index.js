57c1ba3b13ec0ec566497e9fb3bd2035c48ead1d
aytajramazanly <aytajshr@code.edu.az>
2021-12-14 05:11:50 +0400

    side bar width alternative added

--- sideBarJsTask/js/index.js
+++ sideBarJsTask/js/index.js
@@ -6,3 +6,12 @@ btnOpen.addEventListener("click", ()=>{
 btnClose.addEventListener("click",()=>{
     document.querySelector("#side-bar").style.transform="translateX(-100%)"
 })
+
+// let btnOpen=document.querySelector(".open")
+// let btnClose=document.querySelector(".close")
+// btnOpen.addEventListener("click", ()=>{
+//     document.querySelector("#side-bar").style.width="30%"
+// })
+// btnClose.addEventListener("click",()=>{
+//     document.querySelector("#side-bar").style.width="0"
+// })
