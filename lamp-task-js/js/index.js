a0c43051269238dae9d336eeded08cea7a305aa6
aytajramazanly <aytajshr@code.edu.az>
2021-12-14 18:00:17 +0400

    lamp task js change

--- lamp-task-js/js/index.js
+++ lamp-task-js/js/index.js
@@ -1,16 +1,17 @@
-let boxs=Array.from(document.querySelectorAll(".box"))
+let boxs=document.querySelectorAll(".box")
 let btns=document.querySelectorAll("button")
-
-
-btns.forEach((btn)=>{
-    btn.addEventListener("click", ()=>{
-        let databgColor=btn.getAttribute("data-id");
-        boxs.forEach((box)=>{
-            box.style.backgroundColor="white"
-         })
-         document.getElementById(databgColor).style.backgroundColor=databgColor
-    })
-})
+let prebtn;
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      let bgcolor = btn.getAttribute("data-id");
+      if (prebtn) {
+        prebtn.style.backgroundColor = "white";
+      }
+      let box = document.getElementById(bgcolor);
+      prebtn = box;
+      box.style.backgroundColor = bgcolor;
+    });
+  });
 
 
 
--- sideBarJsTask/js/index.js
+++ sideBarJsTask/js/index.js
@@ -1,17 +1,11 @@
 let btnOpen=document.querySelector(".open")
 let btnClose=document.querySelector(".close")
-btnOpen.addEventListener("click", ()=>{
-    document.querySelector("#side-bar").style.transform="translateX(28%)"
+btnOpen.addEventListener("click", (e)=>{
+    document.querySelector("#side-bar").style.width="22%"
+    e.target.style.display="none"
+    
 })
 btnClose.addEventListener("click",()=>{
-    document.querySelector("#side-bar").style.transform="translateX(-100%)"
+    document.querySelector("#side-bar").style.width="0"
+    document.querySelector(".open").style.display="block"
 })
-
-// let btnOpen=document.querySelector(".open")
-// let btnClose=document.querySelector(".close")
-// btnOpen.addEventListener("click", ()=>{
-//     document.querySelector("#side-bar").style.width="30%"
-// })
-// btnClose.addEventListener("click",()=>{
-//     document.querySelector("#side-bar").style.width="0"
-// })
